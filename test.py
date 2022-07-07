7fe7a7787f366092cfb119e452afe60afb06755f
sreejithraju <sreeji.kr@gmail.com>
2022-07-07 18:18:24 +0530

    add

--- test.py
+++ test.py
@@ -1,5 +1,57 @@
-max_executions = 500
-a = []
-for x in range(1,100):
-    a.append(f"name{x}")
-print(a[0:max_executions])
+[
+	{
+		'Attachment': {
+			'AttachTime': datetime.datetime(
+				2022,
+				6,
+				17,
+				10,
+				35,
+				3,
+				tzinfo=tzlocal(
+				)
+			),
+			'AttachmentId': 'eni-attach-0d69941796672c51d',
+			'DeleteOnTermination': True,
+			'DeviceIndex': 0,
+			'NetworkCardIndex': 0,
+			'InstanceId': 'i-0cbd497b02f5ef00a',
+			'InstanceOwnerId': '276110243452',
+			'Status': 'attached'
+		},
+		'AvailabilityZone': 'us-east-1b',
+		'Description': '',
+		'Groups': [
+			{
+				'GroupName': 'launch-wizard-4',
+				'GroupId': 'sg-0c95aa7c3c3b4d050'
+			}
+		],
+		'InterfaceType': 'interface',
+		'Ipv6Addresses': [
+		],
+		'MacAddress': '12:94:d8:ae:8e:d9',
+		'NetworkInterfaceId': 'eni-048a2065955276a7c',
+		'OwnerId': '276110243452',
+		'PrivateDnsName': 'ip-172-31-88-67.ec2.internal',
+		'PrivateIpAddress': '172.31.88.67',
+		'PrivateIpAddresses': [
+			{
+				'Primary': True,
+				'PrivateDnsName': 'ip-172-31-88-67.ec2.internal',
+				'PrivateIpAddress': '172.31.88.67'
+			}
+		],
+		'RequesterManaged': False,
+		'SourceDestCheck': True,
+		'Status': 'in-use',
+		'SubnetId': 'subnet-08f7b48cd2c0e0962',
+		'TagSet': [
+			{
+				'Key': 'Delete_NIC',
+				'Value': '12/02/2022'
+			}
+		],
+		'VpcId': 'vpc-0afa8987794082b8a'
+	}
+]
